f396a5c3a8da9e9e1b8ae6e31fa4a53b7e91aafa
morinaga00063 <95069453+morinaga00063@users.noreply.github.com>
2021-12-01 14:41:25 +0900

    Delete Katacoda_Scenario_Make/test directory

--- Katacoda_Scenario_Make/test/background.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "This is a background script with a long running process"
-
-useradd -m linucuser3 -s /bin/bash
-su - linucuser3
-
-sleep 10
-
-echo "done" >> /opt/.backgroundfinished
--- Katacoda_Scenario_Make/test/step1.md
+++
@@ -1,5 +0,0 @@
-`echo "Run in Terminal 2"`{{execute T2}}
-
-`echo "Run in Terminal 1"`{{execute T1}}
-
-`echo "Run in Terminal 2"`{{execute T2}}
